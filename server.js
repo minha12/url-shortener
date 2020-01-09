69cd67125535d6667c4b40a2c442d595acd65c54
Glitch (murl-shortener) <none>
2020-01-09 10:49:29 +0000

    🕷🍫 Checkpoint ./server.js:9419671/2013 ./package.json:9419671/1090

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-const bodyParser = require('body-parser')
+var bodyParser = require('body-parser')
+var dns = require('dns')
 
 var cors = require('cors');
 
@@ -55,24 +56,38 @@ const URLCollection = mongoose.model('URLCollection', urlSchema)
 app.post('/api/shorturl/new', function(req, res) {
   const originalUrl = req.body.url
   const urlRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
-  const removedProtocolUrl = 
-  console.log(originalUrl)
+  const removedProtocolUrl = originalUrl.replace(/^https?\:\/\//i,'')
+  const domainOnly = removedProtocolUrl.match( /^(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/i )
+  const validUrl = originalUrl.match(urlRegex)
+  console.log(validUrl)
   const randomNumber = Math.floor(Math.random() * 10000 + 1)
-  URLCollection.findOne({original_url: originalUrl}, (err, data) => {
-    if(err) return err
-    if(data) {
-      console.log('The given URL is available in database')
-      res.send(data)
-    } else {
-      console.log('Creating new an url entry for ' + originalUrl)
-      var newEntry = new URLCollection({original_url: originalUrl, shorturl: randomNumber})
-      newEntry.save((err, data) => {
-        if(err) return err
-        res.send(data)
-      })
-    }
-  })
+  
+  if(validUrl){
+    dns.lookup(domainOnly[0], err => {
+      if(err) {
+        res.send('Invalid URL')
+      } else{
+        URLCollection.findOne({original_url: validUrl}, (err, data) => {
+          if(err) return err
+          if(data) {
+            console.log('The given URL is available in database')
+            res.send(data)
+          } else {
+            console.log('Creating new an url entry for ' + originalUrl)
+            var newEntry = new URLCollection({original_url: validUrl, shorturl: randomNumber})
+            newEntry.save((err, data) => {
+              if(err) return err
+              res.send(data)
+            })
+          }
+        })
+      }
+    })
+  }
+  
 })
+
+
 /////////////////////////////////////////////////////////////
 
 app.listen(port, function () {
