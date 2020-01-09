498958918e3cc6775be1231a7ae171e9fc355419
Glitch (murl-shortener) <none>
2020-01-09 10:37:29 +0000

    🐃🏊🏼 Checkpoint ./server.js:9419671/504

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ const URLCollection = mongoose.model('URLCollection', urlSchema)
 //Post a new URL
 app.post('/api/shorturl/new', function(req, res) {
   const originalUrl = req.body.url
+  const urlRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
+  const removedProtocolUrl = 
   console.log(originalUrl)
   const randomNumber = Math.floor(Math.random() * 10000 + 1)
   URLCollection.findOne({original_url: originalUrl}, (err, data) => {
